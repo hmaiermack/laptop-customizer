3ad0188f8c1265629665f47fa9ed05e7b5dd121d
Mack Maier <hmaiermack@gmail.com>
2020-01-25 16:01:41 -0800

    initial commit for thinkful assignment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import React, { Component } from 'react';
 import slugify from 'slugify';
 
 import './App.css';
+import MainForm from './MainForm.js'
+import Summary from './Summary'
+import Total from './Total'
+import Header from './Header'
 
 // This object will allow us to
 // easily convert numbers into US dollar values
@@ -32,9 +36,12 @@ class App extends Component {
         name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
         cost: 1500
       }
-    }
+    },
+    total: null,
   };
 
+
+  //updates selected form element
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
@@ -44,76 +51,34 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
 
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
-
+    //returns whole app
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing | Laptops</h1>
-        </header>
+        <Header />
         <main>
           <form className="main__form">
             <h2>Customize your laptop</h2>
-            {features}
+            <MainForm 
+              features={this.props.features}
+              selected={this.state.selected}
+              updateFeature={this.updateFeature}
+              currency={USCurrencyFormat}>
+            </MainForm>
           </form>
           <section className="main__summary">
-            <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
+            <h2>Your Cart</h2>
+            <Summary 
+            selected={this.state.selected}
+            currency={USCurrencyFormat}>
+            </Summary>
+            <Total
+            currency={USCurrencyFormat}
+            updateFeature={this.updateFeature}
+            total={Object.keys(this.state.selected).reduce(
+              (acc, curr) => acc + this.state.selected[curr].cost,
+              0)}>
+            </Total>
           </section>
         </main>
       </div>
